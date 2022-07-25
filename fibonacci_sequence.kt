e3aa03c1ff03ef09931c73c34a4098613efee70c
JoHnTsIm <84143748+JoHnTsIm@users.noreply.github.com>
2022-07-25 22:05:42 +0300

    add comment & makes some small changes

--- fibonacci_sequence.kt
+++ fibonacci_sequence.kt
@@ -1,9 +1,14 @@
 fun main() {
-    println("type length of fibonacci sequence that you prefer (integer): ")
+    println("\ntype length of fibonacci sequence that you prefer (integer): ")
     val lengthNum: Int = readLine()!!.toInt() - 1
     fibonacciSequence(lengthNum)
 }
 
+// gets an empty list and fill's the list with the fibonacci sequence
+// the length of the list is equal with the length that user types when program starts
+// if the user give's a number <=1 its gonna get the list with just the 1 inside
+// or string instead of integer its gonna get an error back
+// otherwise they will get a filled Fibonacci list of their typed size
 fun fibonacciSequence(lengthNum: Int) {
     val fibList: MutableList<Int> = mutableListOf(1)
     for (num in 1..lengthNum) fibList.add(num)
